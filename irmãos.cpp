a152d457b040889e3c8945edf65f22f6569509c9
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2026-05-06 13:18:10 -0300

    Add files via upload

---
+++ irmãos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    int M;
+    
+    cin >> N >> M;
+    
+    int subidade = M - N;
+    
+    cout <<  M + subidade;
+    
+    return 0;
+}
