d258cc5d8fce84f1223f59f2a0b4d631a82c0069
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2026-05-11 13:20:46 -0300

    Add files via upload

---
+++ flipper.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int P;
+    int R;
+    
+    cin >> P >> R;
+    
+    if(P == 0){
+        cout << C;
+    }
+    
+    
+    return 0;
+}
