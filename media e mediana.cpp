9e328f57fec3a6e0df2e985b76194677d2b71a1f
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2026-05-11 13:11:16 -0300

    Add files via upload

---
+++ media e mediana.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    int B;
+    
+    cin >> A >> B;
+    
+    int R = (2 * A) - B;
+    
+    cout << R;
+
+    return 0;
+}
